a37c0d41bd83795a37f00196117dadf5a4aecccb
Fredy Mateo <fredymateomellado@gmail.com>
2025-06-16 09:20:11 -0500

    Reduciendo archivos y códigos por defecto a lo más básico para poder construir la app desde cero y poder entender la estructura a un nivel mas avanzado

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
+/*Este archivo es el punto de entrada principal de tu aplicación React. Aquí es donde se
+ inicializa y monta el componente raíz*/
+//import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
 import App from './App.jsx'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
