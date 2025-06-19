08588f6c7d78c71269e978c94399cd19d3fcad36
Fredy Mateo <fredymateomellado@gmail.com>
2025-06-19 13:51:58 -0500

    Quitando Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import { useState } from 'react' // Función para manejar estados dentro de los componentes.
 
 /*Desarrollo*/
-import Navbar from './componentes/Navbar';//importamos el componente Navbar
-import Carousel001 from './componentes/Carousel001';//importamos el componente Navbar
+
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <div className="container mt-4">
-        <Carousel001/>
-        <h1>Componentes en desarrollo</h1>
+      <div>
+        <h1>Componentes en desarrollo ahora con MUI</h1>
       </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,11 @@
 /*Este archivo es el punto de entrada principal de tu aplicación React. Aquí es donde se
  inicializa y monta el componente raíz*/
-//import { BrowserRouter } from "react-router-dom"; // Estilos globales
 import { StrictMode } from 'react' //Sirve para detectar errore el el código - solo en desarrollo
 import { createRoot } from 'react-dom/client' //Inicia React en el DOM.
-//import './index.css'//Estilos globales
+
 import App from './App.jsx' //Componente raiz para renderizar la app
 
 /*Desarrollo Fred Mateo*/
-import 'bootstrap/dist/css/bootstrap.min.css'; //importamos bootstrap 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';// imprtación para habilitar dropdowns y el menú hamburguesa.
-
-
-
-
-
 
 /*Montar la aplicacion en REACT en el HTML index.html a partir del id=root*/
 createRoot(document.getElementById('root')).render(
